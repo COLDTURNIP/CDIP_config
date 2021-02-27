bb53ee156a8a432803946c026707e87241c37d53
COLDTURNIP <coldturnip@gmail.com>
2021-02-27 21:45:44 +0800

    remove unneeded configuration

--- conky/scripts/ip.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-exip=`curl -s http://checkip.dyndns.org/ | grep -o "[[:digit:].]\+"`
-echo "$exip"
--- conky/scripts/start_conky.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sleep 20 && conky -c ~/.config/conky/theme/conkyrc -q;
-
